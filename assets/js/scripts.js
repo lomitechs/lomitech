52807197f27dc9ace3373e87a055a2e031d694d9
lomitechs <info@lominat.org>
2024-09-15 15:44:57 -0400

    changes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -38,7 +38,14 @@ function changeSlide(n) {
     slides[slideIndex - 1].style.display = "block"; // Show current slide
 }
 
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
 
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
 
 
 
